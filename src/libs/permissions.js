4442aaf92443c7b021667d74ca2a0fe5ddd9a075
Breezewish <me@breeswish.org>
2016-10-18 20:34:15 +0800

    show round status

--- src/libs/permissions.js
+++ src/libs/permissions.js
@@ -1,6 +1,9 @@
 const permissions = {};
 
-permissions.VIEW_ALL_SUBMISSION = 1 << 0;
-permissions.VIEW_OWN_SUBMISSION = 1 << 1;
+permissions.VIEW_ALL_SUBMISSION         = 1 << 0;
+permissions.VIEW_OWN_SUBMISSION         = 1 << 1;
+permissions.VIEW_MANAGE_PORTAL          = 1 << 2;
+permissions.REFRESH_MATCH_STATUS        = 1 << 3;
+permissions.REFRESH_SUBMISSION_STATUS   = 1 << 4;
 
 export default permissions;
