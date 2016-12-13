98e82558299a9af4f7c8c03dc890c66177f7f17a
Breezewish <me@breeswish.org>
2016-12-13 11:18:32 +0800

    support verify leaderpair

--- src/models/leaderpair.js
+++ src/models/leaderpair.js
@@ -13,17 +13,23 @@ export default () => {
   // LeaderPair Model
   let LeaderPair;
 
-  // Rebuild all pair records
-  LeaderPairSchema.statics.rebuildAsync = async function () {
-    DI.logger.debug('LeaderPair.rebuildAsync: removing all records...');
-    await LeaderPair.remove({});
-    DI.logger.debug('LeaderPair.rebuildAsync: fetching last submissions...');
+  /**
+   * Rebuild all pairs
+   *
+   * @param  {Boolean} verify Only verify integrity and do not change data
+   */
+  LeaderPairSchema.statics.rebuildAsync = async function (verify = false) {
+    if (!verify) {
+      await LeaderPair.remove({});
+    }
     const lsdocs = await DI.models.Submission.getLastSubmissionsByUserAsync();
     const mdocCursor = DI.models.Match.getPairwiseMatchesCursor(_.map(lsdocs, 'sdocid'));
     const mdocCount = await mdocCursor.query.model.count(mdocCursor.query._conditions);
     for (let mdoc = await mdocCursor.next(), i = 1; mdoc !== null; mdoc = await mdocCursor.next(), i++) {
-      DI.logger.debug('LeaderPair.rebuildAsync: updating %d/%d...', i, mdocCount);
-      await LeaderPair.updatePairByMatchAsync(mdoc);
+      if (i % 100 === 0) {
+        DI.logger.debug('LeaderPair.rebuildAsync: %d/%d...', i, mdocCount);
+      }
+      await LeaderPair.updatePairByMatchAsync(mdoc, verify);
     }
     DI.logger.debug('LeaderPair.rebuildAsync: done');
     return true;
@@ -36,20 +42,39 @@ export default () => {
    * @param  {ObjectId} u2id
    * @param  {ObjectId} u1sid u1 submission id
    * @param  {String} status u1 submission status
+   * @param  {Boolean} verify Only verify integrity and do not change data
    */
-  LeaderPairSchema.statics.updatePairAsync = async function (u1id, u2id, u1sid, u1status) {
+  LeaderPairSchema.statics.updatePairAsync = async function (u1id, u2id, u1sid, u1status, verify = false) {
     const u1buf = new Buffer(u1id.toString(), 'hex');
     const u2buf = new Buffer(u2id.toString(), 'hex');
     const u1u2 = Buffer.concat([u1buf, u2buf]);
-    await LeaderPair.update(
-      { u1u2, u1Submission: { $lte: u1sid } },
-      { $set: { u1Submission: u1sid, u1: u1id, u2: u2id, status: u1status } },
-      { upsert: true, runValidators: true }
-    ).exec();
+    if (!verify) {
+      await LeaderPair.update(
+        { u1u2, u1Submission: { $lte: u1sid } },
+        { $set: { u1Submission: u1sid, u1: u1id, u2: u2id, status: u1status } },
+        { upsert: true, runValidators: true }
+      ).exec();
+    } else {
+      const doc = await LeaderPair.findOne({ u1u2 });
+      try {
+        if (doc === null) {
+          throw new Error('pair_doc not found');
+        }
+        if (!doc.u1Submission.equals(u1sid)) {
+          throw new Error('u1Submission not equal, new = ' + u1sid);
+        }
+        if (doc.status !== u1status) {
+          throw new Error('status not equal, new = ', u1status);
+        }
+      } catch (err) {
+        DI.logger.debug('LeaderPair.updatePairAsync: %s (u1 = %s, u2 = %s)', err.message, u1id, u2id);
+        return false;
+      }
+    }
     return true;
   };
-  LeaderPairSchema.statics.updatePairByMatchAsync = function (mdoc) {
-    return LeaderPair.updatePairAsync(mdoc.u1, mdoc.u2, mdoc.u1Submission, mdoc.status);
+  LeaderPairSchema.statics.updatePairByMatchAsync = function (mdoc, verify = false) {
+    return LeaderPair.updatePairAsync(mdoc.u1, mdoc.u2, mdoc.u1Submission, mdoc.status, verify);
   };
 
   /**
