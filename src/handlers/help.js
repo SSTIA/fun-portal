ff8f4224334fef3d0caff8d3ea5d519022e5c18b
tc-imba <liuyh615@sjtu.edu.cn>
2018-03-28 15:30:31 +0800

    add a help section

---
+++ src/handlers/help.js
@@ -0,0 +1,23 @@
+import * as web from 'express-decorators';
+import utils from 'libs/utils';
+import sanitizers from 'libs/sanitizers';
+import permissions from 'libs/permissions';
+
+@web.controller('/help')
+export default class Handler {
+
+  @web.use()
+  async navType(req, res, next) {
+    res.locals.nav_type = 'help';
+    next();
+  }
+
+  @web.get('/rules')
+  async rules(req, res) {
+    res.render('help_rules', {
+      page_title: 'Rules',
+    });
+  }
+
+
+}
